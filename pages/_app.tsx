f699920903c33aa46d1bbce52fa2de6be566f01e
Gor Sargsyan <gor.sargsyan.bb@gmail.com>
2022-08-27 20:51:52 +0400

    working on improvements

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,24 @@ import Layout from '../components/layout/Layout'
 import type { AppProps } from 'next/app'
 import '../styles/globals.scss'
 import React, { useEffect } from 'react'
-import Head from "next/head";
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+import Script from 'next/script'
+import * as ga from '../helpers/google-analytics'
 
 function TheWhitePaperApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      ga.pageView(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   useEffect(() => {
     const observer = new IntersectionObserver(
       function (entries) {
@@ -41,30 +56,47 @@ function TheWhitePaperApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-        <Head>
-            <title></title>
-            <link
-                rel="apple-touch-icon"
-                sizes="180x180"
-                href="/favicon/apple-touch-icon.png"
-            />
-            <link
-                rel="icon"
-                type="image/png"
-                sizes="32x32"
-                href="/favicon/favicon-32x32.png"
-            />
-            <link
-                rel="icon"
-                type="image/png"
-                sizes="16x16"
-                href="/favicon/favicon-16x16.png"
-            />
-            <link rel="manifest" href="/favicon/site.webmanifest" />
-            <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
-            <meta name="msapplication-TileColor" content="#ffffff" />
-            <meta name="theme-color" content="#ffffff" />
-        </Head>
+      <Head>
+        <Script
+          strategy={'afterInteractive'}
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+        ></Script>
+        <Script strategy={'afterInteractive'} id="google-analytics-script">
+          {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                
+                  gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
+            `}
+        </Script>
+        <title></title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#000000"
+        />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
